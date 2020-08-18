5880c99e8dc3274defe3b76fda52ac176d429ef7
Shijie An <shijie.an1249@gmail.com>
2020-08-18 18:11:45 +0800

    upload

--- easy/mergeTwoSortedList/merge.js
+++ easy/mergeTwoSortedList/merge.js
@@ -28,3 +28,30 @@ function listNode(val) {
          return l2;
      }
  }
+
+ //我的第二种方法 
+ //比较繁琐一点
+  const mergeTwoLists2 = function (l1, l2) {
+      const node = new listNode(null);
+      const pointer = node;
+      if (l1 == null) {
+          return l2;
+      }
+      if (l2 == null) {
+          return l1;
+      }
+      if (l1.val < l2.val) {
+          pointer.next = new listNode(l1.val);
+          pointer = pointer.next;
+          l1 = l1.next;
+      } else if (l1.val > l2.val) {
+          pointer.next = new listNode(l2.val);
+          pointer = pointer.next;
+          l2 = l2.next;
+      } else {
+          node.next = new listNode(l1.val);
+          pointer = pointer.next;
+          l1 = l1.next;
+          l2 = l2.next;
+      }
+  }
