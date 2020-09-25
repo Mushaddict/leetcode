056334d54db97ad28482166669fd2953af3e2011
Shijie An <shijie.an1249@gmail.com>
2020-09-25 16:55:13 +0800

    solved

---
+++ easy/0108convertSortedArrayToBinaryTree/convert.js
@@ -0,0 +1,16 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+const convert = function(nums) {
+    if (nums.length === 0) {
+        return null;
+    }
+    const mid = nums.length >> 1;
+    const root = new TreeNode(nums[mid]);
+
+    root.left = convert(nums.slice(0, mid));
+    root.right = convert(nums.slice(mid + 1));
+}
+
