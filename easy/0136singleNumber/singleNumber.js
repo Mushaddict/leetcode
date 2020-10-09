1437531a82a93ded1762e934e919172b24ea679b
Shijie An <shijie.an1249@gmail.com>
2020-10-09 15:14:23 +0800

    solved

---
+++ easy/0136singleNumber/singleNumber.js
@@ -0,0 +1,10 @@
+const singleNumber = nums => {
+    let ret = 0;
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+        ret = ret ^ element;
+    }
+    return ret;
+}
+
+console.log(singleNumber([2, 2, 1]));
