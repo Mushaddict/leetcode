057edbeaefd62befd358686a867ec5496d245e8f
Shijie An <shijie.an1249@gmail.com>
2020-10-12 17:46:05 +0800

    solved

--- easy/0167twoSumArrayisSorted/twoSum.js
+++ easy/0167twoSumArrayisSorted/twoSum.js
@@ -9,9 +9,14 @@ const twoSum = function(numbers, target) {
     for (let index = 0; index < numbers.length; index++) {
         const element = numbers[index];
         if (visited[target - element] !== void 0) {
-            return visited[target - element], index + 1;
+            return [visited[target - element], index + 1];
         }
         visited[element] = index + 1;
     }
     return [];
 }
+
+const numbers = [2, 7, 11, 15];
+const target = 9;
+
+console.log(twoSum(numbers, target));
